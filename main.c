bf6a6e91b5c73e55f308b6295f5303366a50de97
stathismast <st.mastorakis@gmail.com>
2019-01-07 18:29:13 +0200

    Traces are now run together. Added support for the required command line arguments:

--- main.c
+++ main.c
@@ -3,25 +3,44 @@
 #include "parser.h"
 
 int main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("Usage: ./a.out k\n");
+    if(argc < 3){
+        printf("Usage: ./a.out k q\n");
         exit(0);
     }
 
     int k = atoi(argv[1]);
+    int q = atoi(argv[2]);
+    
+    int max = 1000000;
+    if(argc == 4){
+        max = atoi(argv[3]);
+    }
+
+    if(q > max) q = max;
 
     Request * bzip = createRequestArray("./traces/bzip.trace");
-    // Request * gcc = createRequestArray("./traces/gcc.trace");
+    Request * gcc = createRequestArray("./traces/gcc.trace");
 
     PageTable * pt = newPageTable(10, k);
 
-    for(int i=0; i<1000000; i++){
-        addToPageTable(pt, bzip[i].page, bzip[i].rw);
+    int bzipCount = 0;
+    int gccCount = 0;
+    for(int i=0; i<2*max; i++){
+        if((i/q)%2 == 0){
+            addToPageTable(pt, bzip[bzipCount].page, bzip[bzipCount].rw);
+            bzipCount++;
+            printf("bzip %d %d\n", i, bzipCount);
+        }
+        else{
+            addToPageTable(pt, gcc[gccCount].page, gcc[gccCount].rw);
+            gccCount++;
+            printf("gcc %d %d\n", i, gccCount);
+        }
     }
     printPageTable(pt);
 
     deletePageTable(pt);
 
     free(bzip);
-//     free(gcc);
+    free(gcc);
 }
